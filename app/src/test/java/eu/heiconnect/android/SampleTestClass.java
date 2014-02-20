44018669ea81682a04160b2826328592e7c958e3
Louis Davin <louis.developpement@gmail.com>
2014-02-20 22:24:59 +0100

    Tune up build.gradle and add some tests

---
+++ app/src/test/java/eu/heiconnect/android/SampleTestClass.java
@@ -0,0 +1,16 @@
+package eu.heiconnect.android;
+
+import android.test.InstrumentationTestCase;
+
+
+public class SampleTestClass extends InstrumentationTestCase {
+
+    public void testSample() {
+        // Given
+
+        // When
+
+        // Then
+        assertTrue(true);
+    }
+}
