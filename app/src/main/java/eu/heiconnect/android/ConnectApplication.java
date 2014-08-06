748eaa626c4a77f253f5e155c2e67920398826e3
Louis Davin <louis.developpement@gmail.com>
2014-08-06 20:35:40 +0200

    Use OKHttp as http stack

--- app/src/main/java/eu/heiconnect/android/ConnectApplication.java
+++ app/src/main/java/eu/heiconnect/android/ConnectApplication.java
@@ -6,6 +6,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
 import eu.heiconnect.android.utils.Configuration;
+import eu.heiconnect.android.webservice.OkHttpStack;
 
 public class ConnectApplication extends Application {
 
@@ -22,7 +23,7 @@ public class ConnectApplication extends Application {
     public void onCreate() {
         super.onCreate();
         configuration = new Configuration();
-        requestQueue = Volley.newRequestQueue(this);
+        requestQueue = Volley.newRequestQueue(this, new OkHttpStack());
         requestQueue.start();
     }
 
